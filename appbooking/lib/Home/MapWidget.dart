5517b2d1d4c46d773eb937c4bbd437587652bad3
long <longpdh02@gmail.com>
2023-12-08 14:34:10 +0700

    hoạn thiện màn tìm kiếm điểm và nối liên tiếp các điểm

--- appbooking/lib/Home/MapWidget.dart
+++ appbooking/lib/Home/MapWidget.dart
@@ -1,8 +1,8 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_webservice/directions.dart' as directions;
 
 void main() {
   runApp(MyApp());
@@ -24,18 +24,18 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
-  TextEditingController startAddressController = TextEditingController();
-  TextEditingController destinationAddressController = TextEditingController();
+  TextEditingController searchController = TextEditingController();
   Set<Marker> markers = {};
-  Set<Polyline> polylines = {};
-  List<LatLng> polylineCoordinates = [];
+  List<LatLng> allPoints = [];
+  Polyline? polyline;
+  final directions.GoogleMapsDirections _directions = directions.GoogleMapsDirections(apiKey: 'AIzaSyBevJuSc3k1zsDNgrIz4jy_30NMomRgJqQ');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Map Example'),
-      // ),
+      appBar: AppBar(
+        title: Text('Map Example'),
+      ),
       body: Column(
         children: [
           Padding(
@@ -44,22 +44,17 @@ class _MapScreenState extends State<MapScreen> {
               children: [
                 Expanded(
                   child: TextField(
-                    controller: startAddressController,
+                    controller: searchController,
                     decoration: InputDecoration(
-                      labelText: 'Điểm bắt đầu',
+                      labelText: 'Nhập điểm tìm kiếm',
                     ),
                   ),
                 ),
                 SizedBox(width: 10),
-                Expanded(
-                  child: TextField(
-                    controller: destinationAddressController,
-                    decoration: InputDecoration(
-                      labelText: 'Điểm kết thúc',
-                    ),
-                  ),
+                ElevatedButton(
+                  onPressed: () => _onSearchButtonPress(),
+                  child: Text('Tìm kiếm'),
                 ),
-                
               ],
             ),
           ),
@@ -67,16 +62,12 @@ class _MapScreenState extends State<MapScreen> {
             child: GoogleMap(
               onMapCreated: _onMapCreated,
               initialCameraPosition: CameraPosition(
-                target: LatLng(37.7749, -122.4194), // Default: San Francisco
+                target: LatLng(21.005407899999998, 105.8113602),
                 zoom: 12,
               ),
               markers: markers,
-              polylines: polylines,
+              polylines: polyline != null ? {polyline!} : {},
             ),
-          ),
-          ElevatedButton(
-            onPressed: () => _onButtonPress(),
-            child: Text('Tìm đường'),
           ),
         ],
       ),
@@ -84,87 +75,69 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   void _onMapCreated(GoogleMapController controller) {
-    if (!mounted) return; // Check if the widget is still in the tree
     setState(() {
       mapController = controller;
     });
   }
 
-  void _onButtonPress() async {
-    String startAddress = startAddressController.text;
-    String destinationAddress = destinationAddressController.text;
+  void _onSearchButtonPress() async {
+    String searchAddress = searchController.text;
 
-    List<Location> startLocations = await locationFromAddress(startAddress);
-    List<Location> destinationLocations =
-        await locationFromAddress(destinationAddress);
+    List<Location> locations = await locationFromAddress(searchAddress);
+    if (locations.isNotEmpty) {
+      LatLng destination = LatLng(locations[0].latitude, locations[0].longitude);
 
-    LatLng startLatLng =
-        LatLng(startLocations[0].latitude, startLocations[0].longitude);
-    LatLng destinationLatLng = LatLng(
-        destinationLocations[0].latitude, destinationLocations[0].longitude);
-    print("2 Điểm ${startLatLng} ${destinationLatLng}");
-    // Clear existing markers and polylines
-    markers.clear();
-    polylines.clear();
-    polylineCoordinates.clear();
+      // Add marker for the destination
+      markers.add(Marker(
+        markerId: MarkerId('destination'),
+        position: destination,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        infoWindow: InfoWindow(
+          title: 'Destination',
+          snippet: searchAddress,
+        ),
+      ));
 
-    // Add markers for start and destination
-    markers.add(Marker(
-      markerId: MarkerId('start'),
-      position: startLatLng,
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
-      infoWindow: InfoWindow(
-        title: 'Start',
-        snippet: startAddress,
-      ),
-    ));
-
-    markers.add(Marker(
-      markerId: MarkerId('destination'),
-      position: destinationLatLng,
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-      infoWindow: InfoWindow(
-        title: 'Destination',
-        snippet: destinationAddress,
-      ),
-    ));
-
-    // Get polyline coordinates
-    PolylinePoints polylinePoints = PolylinePoints();
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-      'AIzaSyBisRTGrVPSBHcazddG3Lh80Be-n9iNej4', // Replace with your Google Maps API key
-      PointLatLng(startLatLng.latitude, startLatLng.longitude),
-      PointLatLng(destinationLatLng.latitude, destinationLatLng.longitude),
+      // Request directions from current location to destination
+      var currentLocation = allPoints.isNotEmpty ? allPoints.last : LatLng(21.005407899999998, 105.8113602);
+      var response = await _directions.directions(
+      directions.Location( lat: currentLocation.latitude, lng: currentLocation.longitude),
+      directions.Location(lat: destination.latitude, lng: destination.longitude),
+      travelMode: directions.TravelMode.driving,
     );
-    print("Số điểm ${result.points}");
-    if (result.points.isNotEmpty) {
-      result.points.forEach((PointLatLng point) {
-        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+
+
+      // Extract polyline coordinates from the response
+      List<LatLng> polylineCoordinates = [];
+      for (var route in response.routes) {
+        for (var leg in route.legs) {
+          for (var step in leg.steps) {
+            polylineCoordinates.addAll(decodePolyline(step.polyline.points));
+          }
+        }
+      }
+
+      // Draw polyline on the map
+      setState(() {
+        polyline = Polyline(
+          polylineId: PolylineId('route'),
+          color: Colors.blue,
+          points: polylineCoordinates,
+        );
       });
-    }
 
-    // Draw polyline on the map
-    setState(() {
-      Polyline polyline = Polyline(
-        polylineId: PolylineId('route'),
-        color: Colors.blue,
-        points: polylineCoordinates,
+      // Move camera to the destination
+      mapController.animateCamera(
+        CameraUpdate.newLatLngZoom(destination, 14),
       );
-      polylines.add(polyline);
-    });
-
-    // Adjust camera position to show both markers
-    double minLat = min(startLatLng.latitude, destinationLatLng.latitude);
-    double maxLat = max(startLatLng.latitude, destinationLatLng.latitude);
-    double minLng = min(startLatLng.longitude, destinationLatLng.longitude);
-    double maxLng = max(startLatLng.longitude, destinationLatLng.longitude);
-
-    LatLngBounds bounds = LatLngBounds(
-      southwest: LatLng(minLat, minLng),
-      northeast: LatLng(maxLat, maxLng),
-    );
-
-    mapController.animateCamera(CameraUpdate.newLatLngBounds(bounds, 50.0));
+    }
   }
+
+  List<LatLng> decodePolyline(String encoded) {
+  List<LatLng> points = PolylinePoints().decodePolyline(encoded).map((point) {
+    return LatLng(point.latitude, point.longitude);
+  }).toList();
+  return points;
+}
 
 }
